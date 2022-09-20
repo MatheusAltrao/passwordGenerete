6623d4368ed567e6f8f916cece9a6dc5edc1d695
matheus <matheusaltrao2@gmail.com>
2022-09-20 17:01:16 -0400

    add function lvlPassword showPower showPassword

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ input.addEventListener('change', () => {
 
 function randomPassword() {
   const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-  const symbols = ['!', '@', '#', '$', '%']
+  const symbols = ['!', '@', '#', '$',]
   const caractersLower = [
     'a',
     'b',
@@ -58,8 +58,83 @@ function randomPassword() {
   password.innerHTML = list.join('')
 
 
-  let copy = password.innerText
+  let newPassword = password.innerText
 
-  navigator.clipboard.writeText(copy)
-  console.log(copy)
+  navigator.clipboard.writeText(newPassword)
+
+
+  return (caractersUpper, caractersLower, symbols, numbers)
 }
+
+
+function lvlPassword() {
+
+  var password = document.getElementById('inputlvl').value
+  var power = 0
+
+
+  if ((password.length >= 4) && (password.length <= 7)) {
+    power += 10;
+  } else if (password.length > 7) {
+    power += 25;
+  }
+
+  if ((password.length >= 5) && (password.match(/[a-z]+/))) {
+    power += 10;
+  }
+
+  if ((password.length >= 6) && (password.match(/[A-Z]+/))) {
+    power += 20;
+  }
+
+  if ((password.length >= 7) && (password.match(/[@#$%&;*]/))) {
+    power += 25;
+  }
+
+  if (password.match(/([1-9]+)\1{1,}/)) {
+    power += -25;
+  }
+
+  showPower(power);
+
+  document.getElementById('inputlvl').value = ''
+
+}
+
+function showPower(power) {
+  const lvlpass = document.getElementById('lvlpass')
+
+  if (power === 0) {
+    lvlpass.innerHTML = 'Insira a sua senha'
+    lvlpass.style.color = ('#fff')
+  }
+
+  if ((power > 1) && (power < 30)) {
+
+    lvlpass.innerHTML = 'Senha muito fraca'
+    lvlpass.style.color = ('#bb2124')
+
+  } else if ((power >= 30) && (power < 50)) {
+
+    lvlpass.innerHTML = 'Senha fraca'
+    lvlpass.style.color = ('#f0ad4e')
+
+  } else if ((power >= 50) && (power < 70)) {
+
+    lvlpass.innerHTML = 'Senha forte'
+    lvlpass.style.color = ('#5bc0de')
+
+  } else if (power > 70) {
+
+    lvlpass.innerHTML = 'Senha muito forte'
+    lvlpass.style.color = ('#22bb33')
+  }
+}
+
+function showPassword() {
+  const password = document.getElementById('inputlvl')
+
+  password.type = password.type == 'text' ? 'password' : 'text'
+
+}
+
